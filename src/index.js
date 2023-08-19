f235cd0c28f8024072d8b83bf0d884e2bf83c6b1
yanaivantsova <yanaivantsova@yandex.by>
2023-08-19 20:03:13 +0200

    feat: add task solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const obj = Object.fromEntries(bracketsConfig);
+  for (let i = 0; i < str.length; i += 1) {
+    let openBracket = str[i];
+    let closeBracket = str[i + 1];
+    if (obj[openBracket] == undefined || closeBracket == undefined) {
+      return false;
+    }
+    if (obj[openBracket] == closeBracket) {
+      str = str.slice(0, i) + str.slice(i + 2, str.length)
+      i = -1;
+    }
+    if (str.length % 2 != 0) {
+      return false
+    }
+  }
+  return true
 }
+
+
+
+
+
+
+
+
